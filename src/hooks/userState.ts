b914d2378920357c0a04d8e98fb3e5d1a75a1ed1
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-08-31 01:48:44 +0900

    [Feat]: 로그인 상태관리

--- src/hooks/userState.ts
+++ src/hooks/userState.ts
@@ -6,26 +6,35 @@ import {emailLogin} from "@/api/auth/auth.api.ts";
 import secureLocalStorage from "react-secure-storage";
 import {ACCESS_TOKEN, REFRESH_TOKEN} from "@/const/data.ts";
 import {LoginResponse} from "@/api/auth/auth.response.ts";
+import {EmailLoginRequest} from "@/api/auth/auth.request.ts";
 
 export interface UserState {
   user?: UserInfoModel;
   error: ApiError | null;
-  login: (email: string, password: string) => Promise<void>;
+  login: (req:EmailLoginRequest) => Promise<void>;
   logout: () => void;
 }
 
 export function useUserState(): UserState {
   const {data, error} = useQuery<UserInfoModel, ApiError>({
     queryKey: ['userInfo'],
+    enabled: secureLocalStorage.getItem(ACCESS_TOKEN) !== null, // 토큰이 없으면 쿼리를 실행x
     queryFn: getUserInfo,
+    staleTime: 1000 * 60 * 60, // 1시간동안 캐시 유지
   });
 
   const queryClient = useQueryClient();
+  //onMutate는 mutationFn이 실행되기 전에 호출되는 함수입니다.
+  //onSettled는 finally와 비슷한 역할을 합니다.
+  //onSuccess는 mutationFn이 성공적으로 실행된 후 호출되는 함수입니다.
+  //onError는 mutationFn이 에러를 반환했을 때
+  //mutate는 onSuccess, onError을 통해 성공했을시, 실패했을시 response 데이터를 핸들링
+
   //useMutation의 제네릭은 다음과 같습니다
-  //1. 성공 시 반환되는 데이터의 타입
+  //1. 성공 시 반환되는 데이터의 타입 (data, onSuccess에서 반환하는 데이터)
   //2. 실패 시 반환되는 에러의 타입
-  //3. mutationFn에 전달되는 데이터의 타입
-  //4. onMutate에서 반환되는 데이터의 타입. 여기서 반환하는 데이터는 onSettled에서 사용할 수 있습니다.
+  //3. mutationFn에 전달되는 데이터의 타입(onSuccess, onError, onMutate, onSettled)
+  //4. onMutate에서 반환되는 데이터의 타입. (onSuccess, onError, onSettled)에서 활용
   const userMutation = useMutation<void, ApiError, void, void>({
     mutationFn: async () => {
       console.log('mutationFn');
@@ -43,18 +52,15 @@ export function useUserState(): UserState {
   const loginMutation = useMutation<
     LoginResponse,
     ApiError,
-    {email: string, password: string},
+    EmailLoginRequest,
     LoginResponse
   >({
-    mutationFn: async ({email, password}) => {
-      return await emailLogin({email, password});
-    },
+    mutationFn: emailLogin,
     onSuccess: async (response) => {
       secureLocalStorage.setItem(ACCESS_TOKEN, response.accessToken);
       secureLocalStorage.setItem(REFRESH_TOKEN, response.refreshToken);
       await queryClient.setQueryData(['userInfo'], response.userInfo);
-      return response;
-    }
+    },
   });
 
 
@@ -63,9 +69,8 @@ export function useUserState(): UserState {
     logout: () => {
       userMutation.mutate();
     },
-    login: async (email: string, password: string) => {
-      await loginMutation.mutateAsync({email, password});
-      return
+    login: async (req:EmailLoginRequest) => {
+      await loginMutation.mutateAsync(req);
     },
     error: error,
   };
