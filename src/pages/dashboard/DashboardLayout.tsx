32b97f9f9665f4483026d217038cbf8f905b7058
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-08-30 19:39:14 +0900

    [Feat]: 유저 hook으로 변경

--- src/pages/dashboard/DashboardLayout.tsx
+++ src/pages/dashboard/DashboardLayout.tsx
@@ -1,24 +1,18 @@
 import {Outlet, useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 import secureLocalStorage from "react-secure-storage";
-import {getUserInfo} from "@/api/user/user.api.ts";
-import {useQuery} from "@tanstack/react-query";
-import {UserInfoModel} from "@/api/user/user.response.ts";
-import {ApiError} from "@/api/ApiError.ts";
+import {useUserState} from "@/hooks/userState.ts";
 
 export default function DashboardLayout() {
   const nav = useNavigate();
-  const { data,error} = useQuery<UserInfoModel, ApiError>({
-    queryKey: ['userInfo'],
-    queryFn: getUserInfo,
-  });
+  const {user, error} = useUserState();
 
   useEffect(() => {
     if(secureLocalStorage.getItem('accessToken') === null) {
       nav('/login');
       return;
     }
-    if(data && data.id <0){//TODO 어드민이 아닐때, 권한 검사
+    if(user && user.id <0){//TODO 어드민이 아닐때, 권한 검사
       nav('/');
       return;
     }
