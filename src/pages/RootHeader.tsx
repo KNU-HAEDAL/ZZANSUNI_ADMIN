47b8f707e689ddc072eda5fc84439f96bb06ea65
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-08-31 14:12:51 +0900

    [Feat]: 유저정보 헤더 추가

--- src/pages/RootHeader.tsx
+++ src/pages/RootHeader.tsx
@@ -3,6 +3,13 @@ import {useUserState} from "@/hooks/userState.ts"
 import {ROUTE_PATH} from "@/const/data.ts";
 import {UserInfoModel} from "@/api/user/user.response.ts";
 import {ApiError} from "@/api/ApiError.ts";
+import {
+  DropdownMenu,
+  DropdownMenuContent, DropdownMenuItem,
+  DropdownMenuLabel, DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 const menuItems = [
   {label: '대시보드', href: ROUTE_PATH.DASHBOARD_HOME},
@@ -89,15 +96,46 @@ function UserMenu({user, logout, error}: UserMenuProps) {
   }
   return (
     <div className="flex flex-row">
-      <img
-        className="w-8 h-8 rounded-full overflow-hidden"
-        src={user.profileImageUrl}
-        alt=""
-      />
+      <UserDropMenu user={user}/>
       <div className="w-4"/>
       <button onClick={logoutButtonClick}>
         로그아웃
       </button>
     </div>
   )
+}
+
+
+function UserDropMenu({user}: {
+  user: UserInfoModel;
+}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <img
+            className="w-8 h-8 rounded-full overflow-hidden"
+            src={user.profileImageUrl === null ? '/logo.svg' : user.profileImageUrl}
+            alt=""
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>유저정보</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          ID: {user.id}
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          티어: {user.tierInfo.tier} {user.tierInfo.currentExp} / {user.tierInfo.totalExp}
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          닉네임: {user.nickname}
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          이메일: {user.email}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
