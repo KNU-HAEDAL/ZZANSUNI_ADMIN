7e9d3e6d05d46d77bbad875fb342e513484df323
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-08-31 21:21:32 +0900

    [Feat]: react-error-boundary 추가

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,13 +1,20 @@
 import RootHeader from "@/pages/RootHeader.tsx";
 import {Outlet} from "react-router-dom";
+import {Suspense} from "react";
+import {ErrorBoundary} from "react-error-boundary";
+
 export default function RootLayout() {
 
   return (
     <div className="w-dvw flex flex-col items-center bg-primary-bg-grey">
       <RootHeader/>
-      <div className="w-dvw h-[calc(auto-80px)] pt-[80px]">
-        <Outlet/>
-      </div>
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <Suspense fallback={"loading.."}>
+          <div className="w-dvw h-[calc(auto-80px)] pt-[80px]">
+            <Outlet/>
+          </div>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
